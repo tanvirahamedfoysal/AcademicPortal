9ef1bb87b307f7a1af8861da07530229dbb53258
Md Mahruf Alam <baisakh2015@gmail.com>
2026-07-17 20:20:33 +0600

    v18

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import QueryProvider from '../providers/QueryProvider';
-import '@/app/globals.css';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
