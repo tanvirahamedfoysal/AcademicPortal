c2883f85b43d9c972fa1cf719dbc0fa9298fd152
Md Mahruf Alam <baisakh2015@gmail.com>
2026-07-17 19:59:36 +0600

    reboot

--- Frontend/components/dashboard/StatCard.tsx
+++
@@ -1,8 +0,0 @@
-export default function StatCard({title,value}:{title:string,value:string}){
-return <div className="panel">
-<p className="text-slate-500">{title}</p>
-<h2 className="text-4xl font-bold text-blue-600 mt-3">
-{value}
-</h2>
-</div>
-}
